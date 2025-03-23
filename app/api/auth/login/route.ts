89b679dde0885f72a5a28034b893035aa29cea31
JonathanPR0 <jonathan.almeida1793@gmai.com>
2025-03-23 13:02:06 -0300

    fix: remove auth middleware

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -24,17 +24,7 @@ export async function POST(req: Request) {
       process.env.JWT_SECRET || "seu_secret_aqui"
     );
 
-    // Salvar o token no cookie com a API de cookies do Next.js
-    const response = NextResponse.json({ token, user }, { status: 200 });
-
-    response.cookies.set("token", token, {
-      httpOnly: true, // Impede o acesso ao cookie via JavaScript (melhora a segurança)
-      // secure: process.env.NODE_ENV === "production", // Só envia o cookie em HTTPS
-      sameSite: "strict", // Protege contra ataques CSRF
-      maxAge: 100 * 365 * 24 * 60 * 60, // Expira em 100 anos (em segundos)
-    });
-
-    return response;
+    return NextResponse.json({ token, user }, { status: 200 });
   } catch (error: any) {
     return NextResponse.json({ error: "Erro ao realizar login", details: error }, { status: 500 });
   }
